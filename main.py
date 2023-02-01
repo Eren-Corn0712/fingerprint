e1563c608ca3d3e7671a5cce7743d6547bfe819d
Corn <awsed10234zx@gmail.com>
2023-02-01 23:50:48 +0800

    UpData

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from engine.matcher import BaseMatcher
+from data.base import BaseDataset
 from utils import LOGGER
 
 
@@ -13,8 +14,13 @@ class Test(object):
         self.console.info("Base Matcher is OK.")
 
     def test_base_dataset(self):
+        self.console.info("Test Base Dataset")
+        base_dataset = BaseDataset("FingerPrintDataset")
+        self.console.info("Test Base Dataset is OK")
+
     def __call__(self, *args, **kwargs):
         self.test_base_matcher()
+        self.test_base_dataset()
 
 
 if __name__ == '__main__':
