b5d229f10b29591734407557893f0089d20f1004
Corn <awsed10234zx@gmail.com>
2023-02-01 21:33:54 +0800

    UpData

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from engine.matcher import BaseMatcher
+from utils import LOGGER
+
+
+class Test(object):
+    def __init__(self,
+                 ):
+        self.console = LOGGER
+
+    def test_base_matcher(self):
+        self.console.info("Test Base Matcher")
+        base_matcher = BaseMatcher()
+        self.console.info("Base Matcher is OK.")
+
+    def __call__(self, *args, **kwargs):
+        self.test_base_matcher()
+
 
 if __name__ == '__main__':
-    base_matcher = BaseMatcher()
+    Test()()
