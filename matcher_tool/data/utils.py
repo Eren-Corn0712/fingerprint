8d2f48e69a9aa02f40992842c52cd5063806b409
corn <awsed10234zx@gmail.com>
2023-02-20 11:45:46 +0800

    lab branch update

--- matcher_tool/data/utils.py
+++ matcher_tool/data/utils.py
@@ -5,14 +5,15 @@ import subprocess
 import time
 from pathlib import Path
 from tarfile import is_tarfile
+from typing import List
 from zipfile import is_zipfile
-
+from tarfile import is_tarfile
 import cv2
 import numpy as np
 import torch
 from PIL import ExifTags, Image, ImageOps
 
-HELP_URL = "See https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data"
+HELP_URL = "You can ask developers."
 IMG_FORMATS = "bmp", "dng", "jpeg", "jpg", "mpo", "png", "tif", "tiff", "webp", "pfm"  # include image suffixes
 VID_FORMATS = "asf", "avi", "gif", "m4v", "mkv", "mov", "mp4", "mpeg", "mpg", "ts", "wmv"  # include video suffixes
 LOCAL_RANK = int(os.getenv("LOCAL_RANK", -1))  # https://pytorch.org/docs/stable/elastic/run.html
